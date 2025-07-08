4ae1c128747d32215308dd7c75de9a9bc4624d16
AndresGarcia-15 <teherana@utb.edu.co>
2025-07-08 11:36:45 -0500

    actualizacion de endopoint

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import requests
 from scheduler import schedule_whatsapp_reminder
 
 app = FastAPI()
 
-# URL pública de tu bot de WhatsApp en Render (ajústala después del despliegue)
 BOT_URL = "https://whatsapp-bot-fastapi-a7hff4cwabftbbcw.canadacentral-01.azurewebsites.net/send"
+GROUPS_URL = "https://whatsapp-bot-fastapi-a7hff4cwabftbbcw.canadacentral-01.azurewebsites.net/groups"
 
 class Reminder(BaseModel):
     phone_number: str
@@ -15,3 +16,11 @@ class Reminder(BaseModel):
 def create_reminder(reminder: Reminder):
     schedule_whatsapp_reminder(reminder, BOT_URL)
     return {"status": "scheduled"}
+
+@app.get("/groups/")
+def get_groups():
+    try:
+        response = requests.get(GROUPS_URL)
+        return response.json()
+    except Exception as e:
+        return {"error": str(e)}
